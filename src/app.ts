7d25f422352e32d072f0ba6353d246ad6653bd43
Henry <hangsu1985@gmail.com>
2019-06-11 22:35:52 +0800

    add index

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.set("port", port);
-app.set("veiews", path.join(__dirname, "../views"));
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "pug");
 
-app.get("/", (_, res) => res.send("Hello World!"));
+app.get("/", (_, res) => res.render("index"));
 
 export default app;
