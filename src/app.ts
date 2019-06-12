aaa8bb72e70ca92880b66fd07fdb5acac721bc07
Henry <hangsu1985@gmail.com>
2019-06-12 10:52:22 +0800

    move views into src folder

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.set("port", port);
-app.set("views", path.join(__dirname, "../views"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.get("/", (_, res) => res.render("index"));
